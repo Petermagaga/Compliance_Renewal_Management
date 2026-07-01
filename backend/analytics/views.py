de2938dd84ed48c858f37c806033e91255981f8a
petermagaga <ptrmagaga@gmail.com>
2026-07-01 15:56:32 +0300

    permissions

--- backend/analytics/views.py
+++ backend/analytics/views.py
@@ -1,11 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from compliance.models import ComplianceItem
-
+from rest_framework.permissions import IsAuthenticated
 
 
 class DashboardStatsView(APIView):
-
+    permission_classes=[IsAuthenticated]
     def get(self,request):
         total=ComplianceItem.objects.count()
         expired=ComplianceItem.objects.filter(status='expired').count()
--- backend/compliance/views.py
+++ backend/compliance/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .serializers import ReminderLogSerializer,ComplianceItemSerializer
 from .models import ComplianceItem,ReminderLog
 
 
 class ComplianceItemViewSet(viewsets.ModelViewSet):
+    permission_classes= [IsAuthenticated]
     queryset=ComplianceItem.objects.all()
     serializer_class=ComplianceItemSerializer
 
 class ReminderLogViewset(viewsets.ModelViewSet):
+    permission_classes=[IsAuthenticated]
     queryset=ReminderLog.objects.all()
     serializer_class=ReminderLogSerializer
     
