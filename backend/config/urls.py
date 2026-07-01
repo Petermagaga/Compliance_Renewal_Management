1f84cd304d93784ac2369a8730f5adb95f86b4fd
petermagaga <ptrmagaga@gmail.com>
2026-07-01 15:47:08 +0300

    urls-core

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework_simplejwt.views import(
+    TokenObtainPairView,TokenRefreshSlidingView
+)
 
 urlpatterns = [
+    path("api/token/",TokenObtainPairView.as_view(),name="token_obtain_pair"),
+    path("api/token/refresh/",TokenRefreshSlidingView.as_view(),name="token_refresh"),
     path('admin/', admin.site.urls),
     path('api/compliance/',include('compliance.urls') ),
     path('api/dashboard/',include('analytics.urls')),
