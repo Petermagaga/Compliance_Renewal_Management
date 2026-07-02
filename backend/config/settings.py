9d24757cf4b4dacbad5192c39ed1b2df5e0387b1
petermagaga <ptrmagaga@gmail.com>
2026-07-02 10:13:40 +0300

    supabase__

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,5 +1,6 @@
 from decouple import config
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -71,10 +72,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=config("DATABASE_URL"))
 }
 
 
@@ -121,13 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT=BASE_DIR/ "staticfiles"
+STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = (
     "whitenoise.storage.CompressedManifestStaticFilesStorage"
 )
-CORS_ALLOW_ALL_ORIGINS = [
+
+CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
 
@@ -136,3 +136,5 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
