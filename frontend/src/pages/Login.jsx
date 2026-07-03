e88f4f33467e594b19800e62e2a75a7c3f4a9c92
petermagaga <ptrmagaga@gmail.com>
2026-07-03 11:24:56 +0300

    api-routing-jwt

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,48 @@
+import { useState } from "react";
+
 function Login() {
-    return(
-        <h1>Login Page</h1>
-    );
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{
+          width: "350px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "15px",
+        }}
+      >
+        <h2>Compliance Management Login</h2>
+
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
