954cdab8b30757df18225afb0bcc0288c63a4afd
petermagaga <ptrmagaga@gmail.com>
2026-07-03 11:04:12 +0300

    api-routing-jwt

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,4 +4,20 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access");
+
+    if (token) {
+      config.headers.Authorization =`Bearer ${token}`;
+    }
+    return config
+
+  },
+  (error) => Promise.reject(error)
+);
+
+
+
 export default api;
