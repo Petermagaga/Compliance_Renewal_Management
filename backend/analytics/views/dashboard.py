fbe8f0a446149c4eef329a2fbd15a4751c7b9023
petermagaga <ptrmagaga@gmail.com>
2026-07-08 08:42:59 +0300

    backend -->dashboard.py

--- backend/analytics/views/dashboard.py
+++ backend/analytics/views/dashboard.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from core.responses import ApiResponse
 from accounts.models import Company
-from serializers.dashboard  import DashboardSerializer
+from serializers.dashboard import DashboardSerializer
 
 class DashboardAPIView(APIView):
 
