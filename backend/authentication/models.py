822a0e5ef238320b1888b6cec2a9b91fb53584b8
petermagaga <ptrmagaga@gmail.com>
2026-07-08 15:12:19 +0300

    authentication

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from accounts.models import Company,Department
-from settings.user import AbstractUser
+from django.contrib.auth.base_user import AbstractUser
 
 class User(AbstractUser):
     company=models.ForeignKey(Company)
