a11e33b3905594b12f8b590878271d517d4b9333
petermagaga <ptrmagaga@gmail.com>
2026-07-17 10:04:29 +0300

    compliance-notifications

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from config.settings import
 
-# Create your models here.
+class Notification(models.Model):
+    recipient=models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,related_name="notifications"
+    )
+
+    title=models.CharField(max_length=255)
+    message=models.TextField()
+    channel=models.CharField(max_length=255)
+    status=models.CharField(max_length=255)
+    is_read=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    
