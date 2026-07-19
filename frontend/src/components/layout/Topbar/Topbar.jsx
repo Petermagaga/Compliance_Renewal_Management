7611f7aa74568857ff6927c2ec84d037d882f956
petermagaga <ptrmagaga@gmail.com>
2026-07-19 21:46:47 +0300

    frontend-notifications

---
+++ frontend/src/components/layout/Topbar/Topbar.jsx
@@ -0,0 +1,18 @@
+import NotificationBell from
+"../features/notifications/components/NotificationBell";
+
+function TopBar() {
+
+    return (
+
+        <header>
+
+            ...
+
+            <NotificationBell />
+
+        </header>
+
+    );
+
+}
