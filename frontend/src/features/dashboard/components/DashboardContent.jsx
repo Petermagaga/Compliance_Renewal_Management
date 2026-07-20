4d57b9e98c864490f40169cf4fca5054fa8dd839
petermagaga <ptrmagaga@gmail.com>
2026-07-20 15:42:52 +0300

    frontend-Dashboard-useDashboard

---
+++ frontend/src/features/dashboard/components/DashboardContent.jsx
@@ -0,0 +1,47 @@
+import DashboardHeader from "./DashboardHeader";
+import KPISection from "./KPISection";
+import ChartsSection from "./ChartsSection";
+import ComplianceSection from "./ComplianceSection";
+import ActivitySection from "./ActivitySection";
+import SystemHealthCard from "./SystemHealthCard";
+import QuickActionsSection from "./QuickActionsSection";
+
+function DashboardContent() {
+
+    return (
+
+        <div className="space-y-8">
+
+            <DashboardHeader />
+
+            <KPISection />
+
+            <ChartsSection />
+
+            <div className="grid grid-cols-1 xl:grid-cols-12 gap-6">
+
+                <div className="xl:col-span-8">
+
+                    <ComplianceSection />
+
+                </div>
+
+                <div className="xl:col-span-4 space-y-6">
+
+                    <ActivitySection />
+
+                    <SystemHealthCard />
+
+                </div>
+
+            </div>
+
+            <QuickActionsSection />
+
+        </div>
+
+    );
+
+}
+
+export default DashboardContent;
