3f708f9416a0e136ffa1f6aa252126e03e2647bf
petermagaga <ptrmagaga@gmail.com>
2026-07-20 10:34:41 +0300

    frontend-dashboard

---
+++ frontend/src/features/dashboard/components/ActivitySection.jsx
@@ -0,0 +1,5 @@
+const {
+
+    recentActivity
+
+} = useDashboard();
---
+++ frontend/src/features/dashboard/components/ChartsSection.jsx
@@ -0,0 +1,27 @@
+import { useDashboard } from "../context/DashboardContext";
+
+function ChartsSection(){
+
+    const { charts } = useDashboard();
+
+    return(
+
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-6">
+
+            <StatusPieChart
+
+                data={charts.status_distribution}
+
+            />
+
+            <ExpiryBarChart
+
+                data={charts.monthly_expiry_trend}
+
+            />
+
+        </div>
+
+    );
+
+}
