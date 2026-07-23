94a9632db57beeafba5ea8e398c7ad5bcf3372d7
petermagaga <ptrmagaga@gmail.com>
2026-07-23 14:58:46 +0300

    sidebar-navigation

--- backend/authentication/serializers/password_serializer.py
+++ backend/authentication/serializers/password_serializer.py
@@ -7,7 +7,7 @@ class ChangePasswordSerializer(serializers.Serializer):
 
     current_password=serializers.CharField(write_only=True)
     new_password=serializers.CharField(write_only=True,min_length=8)
-    confirm_password=serializers.CharField(write_onl=True,min_length=8)
+    confirm_password=serializers.CharField(write_only=True,min_length=8)
 
     def validate(self, attrs):
         
