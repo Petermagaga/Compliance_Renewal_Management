7e859b57f42233f1d90a38cde0918f4febceb94a
petermagaga <ptrmagaga@gmail.com>
2026-07-28 12:58:41 +0300

    Audit

--- backend/audit/views.py
+++ backend/audit/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Activity
+from .serializers import ActivitySerializer
+
+
+class ActivityViewSet(viewsets.ReadOnlyModelViewSet):
+
+    serializer_class = ActivitySerializer
+
+    permission_classes = [IsAuthenticated]
+
+    queryset = Activity.objects.all()[:50]
