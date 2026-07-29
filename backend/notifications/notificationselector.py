cc410ed32baa2f8f122fbc3f7bdaf4ed1b0d47ed
petermagaga <ptrmagaga@gmail.com>
2026-07-29 11:04:12 +0300

    Notification_service-view

---
+++ backend/notifications/notificationselector.py
@@ -0,0 +1,25 @@
+class NotificationSelector:
+
+    @staticmethod
+    def for_user(user):
+        ...
+
+    @staticmethod
+    def unread(user):
+        ...
+
+    @staticmethod
+    def unread_count(user):
+        ...
+
+    @staticmethod
+    def today(user):
+        ...
+
+    @staticmethod
+    def emails(user):
+        ...
+
+    @staticmethod
+    def whatsapp(user):
+        ...
