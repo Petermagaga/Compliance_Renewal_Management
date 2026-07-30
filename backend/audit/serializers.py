df12ee2a209126953dc752b68083acd2fa4d60be
petermagaga <ptrmagaga@gmail.com>
2026-07-30 10:04:42 +0300

    Activityfeed-Backend serializer

--- backend/audit/serializers.py
+++ backend/audit/serializers.py
@@ -1,12 +1,35 @@
-
 from rest_framework import serializers
+
 from .models import Activity
 
 
 class ActivitySerializer(serializers.ModelSerializer):
+    user_name = serializers.SerializerMethodField()
+    user_role = serializers.SerializerMethodField()
 
     class Meta:
-
         model = Activity
+        fields = [
+            "id",
+            "activity_type",
+            "title",
+            "description",
+            "user",
+            "user_name",
+            "user_role",
+            "created_at",
+        ]
 
-        fields = "__all__"
+    def get_user_name(self, obj):
+        if not obj.user:
+            return "System"
+
+        return obj.user.full_name
+
+    def get_user_role(self, obj):
+        if not obj.user:
+            return None
+
+        return obj.user.role
+
+    
