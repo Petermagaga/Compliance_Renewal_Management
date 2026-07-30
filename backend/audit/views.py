c48c57c0bbb41cd6f5b12151130ba270cbaab5f5
petermagaga <ptrmagaga@gmail.com>
2026-07-30 10:06:03 +0300

    Activityfeed-Backend serializer

--- backend/audit/views.py
+++ backend/audit/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
@@ -11,7 +8,18 @@ from .serializers import ActivitySerializer
 class ActivityViewSet(viewsets.ReadOnlyModelViewSet):
 
     serializer_class = ActivitySerializer
-
     permission_classes = [IsAuthenticated]
 
-    queryset = Activity.objects.all()[:50]
+    def get_queryset(self):
+        user = self.request.user
+
+        queryset = Activity.objects.select_related(
+            "user"
+        ).order_by("-created_at")
+
+        if user.company_id:
+            queryset = queryset.filter(
+                user__company_id=user.company_id
+            )
+
+        return queryset[:50]
