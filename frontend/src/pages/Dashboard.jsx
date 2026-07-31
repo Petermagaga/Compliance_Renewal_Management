d4f267929dabaea5281a2c36611514e21078fd54
petermagaga <ptrmagaga@gmail.com>
2026-07-31 11:00:58 +0300

    Notification_service

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,34 +1,71 @@
 import MainLayout from "../components/layout/MainLayout";
 
-import { DashboardProvider }
-from "../features/dashboard/context/DashboardContext";
-
-import DashboardContent
-from "../features/dashboard/components/DashboardContent";
-
-
-
-
+import DashboardHeader from "../features/dashboard/components/DashboardHeader";
+import KPISection from "../features/dashboard/components/KPISection";
+import ChartsSection from "../features/dashboard/components/ChartsSection";
+import ComplianceSection from "../features/dashboard/components/ComplianceSection";
+import ActivitySection from "../features/dashboard/components/ActivitySection";
+import SystemHealthCard from "../features/dashboard/components/SystemHealthCard";
+import QuickActionsSection from "../features/dashboard/components/QuickActionsSection";
 
 function Dashboard() {
-
-
-
-
     return (
-
         <MainLayout>
 
-            <DashboardProvider>
+            <main className="min-h-screen bg-slate-50">
 
-                <DashboardContent />
+                <div className="mx-auto max-w-[1600px] p-6 lg:p-8">
 
-            </DashboardProvider>
+                    {/* Header */}
+
+                    <DashboardHeader />
+
+                    {/* KPI */}
+
+                    <section className="mt-6">
+                        <KPISection />
+                    </section>
+
+                    {/* Charts */}
+
+                    <section className="mt-6">
+                        <ChartsSection />
+                    </section>
+
+                    {/* Compliance + Health */}
+
+                    <section className="mt-6 grid grid-cols-1 gap-6 xl:grid-cols-12">
+
+                        <div className="xl:col-span-8">
+                            <ComplianceSection />
+                        </div>
+
+                        <div className="xl:col-span-4">
+                            <SystemHealthCard />
+                        </div>
+
+                    </section>
+
+                    {/* Activity + Actions */}
+
+                    <section className="mt-6 grid grid-cols-1 gap-6 xl:grid-cols-12">
+
+                        <div className="xl:col-span-8">
+                            <ActivitySection />
+                        </div>
+
+                        <div className="xl:col-span-4">
+                            <QuickActionsSection />
+                        </div>
+
+                    </section>
+
+                </div>
+
+            </main>
 
         </MainLayout>
-
     );
-
 }
 
 export default Dashboard;
