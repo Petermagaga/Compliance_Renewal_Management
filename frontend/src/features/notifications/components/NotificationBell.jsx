e7ebde38a352e29e7a952da9ba892192532fc8a1
petermagaga <ptrmagaga@gmail.com>
2026-08-03 12:18:20 +0300

    polish-dashboard

--- frontend/src/features/notifications/components/NotificationBell.jsx
+++ frontend/src/features/notifications/components/NotificationBell.jsx
@@ -13,39 +13,40 @@ function NotificationBell() {
 
         <div className="relative">
 
-            <button
-                onClick={() => setOpen(!open)}
-                className="relative p-2 rounded-lg hover:bg-gray-100"
-            >
+        <button
+            onClick={() => setOpen(!open)}
+            className="
+                relative
+                rounded-xl
+                p-2
+                transition
+                hover:bg-slate-100
+            "
+        >
+            <Bell size={21} />
 
-                <Bell size={22} />
-
-                {unreadCount > 0 && (
-
-                    <span
-                        className="
-                            absolute
-                            -top-1
-                            -right-1
-                            min-w-[20px]
-                            h-5
-                            px-1
-                            flex
-                            items-center
-                            justify-center
-                            rounded-full
-                            bg-red-500
-                            text-white
-                            text-xs
-                            font-semibold
-                        "
-                    >
-                        {unreadCount}
-                    </span>
-
-                )}
-
-            </button>
+            {unreadCount > 0 && (
+                <span
+                    className="
+                        absolute
+                        -top-2
+                        -right-2
+                        flex
+                        h-5
+                        w-5
+                        items-center
+                        justify-center
+                        rounded-full
+                        bg-red-500
+                        text-[11px]
+                        font-semibold
+                        text-white
+                    "
+                >
+                    {unreadCount}
+                </span>
+            )}
+        </button>
 
             {open && (
 
