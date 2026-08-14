b575f29a7a10c621ecaa069a3f9f40209babf9d5
petermagaga <ptrmagaga@gmail.com>
2026-08-14 09:39:42 +0300

    Reminders-app-Frontend

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -2,11 +2,20 @@ from rest_framework import serializers
 from .models import Company,Department
 
 class CompanySerializer(serializers.ModelSerializer):
-
-
     class Meta:
         model=Company
-        fields='__all__'
+        fields=[
+            "id",
+            "name",
+            "email",
+            "phone",
+            "created_at",
+        ]
+
+        read_only_fields=[
+            "id",
+            "created_at"
+        ]
 
 
 class DepartmentSearializers(serializers.ModelSerializer):
