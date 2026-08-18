aa81e042f649ab59a0a6f38d8cf5c84ddb08878a
petermagaga <ptrmagaga@gmail.com>
2026-08-18 10:47:36 +0300

    login

--- frontend/src/features/dashboard/components/QuickActionsSection.jsx
+++ frontend/src/features/dashboard/components/QuickActionsSection.jsx
@@ -1,45 +1,24 @@
 import DashboardSection from "./DashboardSection";
 import ActionCard from "./ActionCard";
 
-import { dashboardActions }
-from "../config/dashboardActions";
+import { dashboardActions } from "../config/dashboardActions";
 
 function QuickActionsSection() {
-
     return (
-
         <DashboardSection
             title="Quick Actions"
             subtitle="Common compliance tasks"
         >
-
-            <div
-                className="
-                    grid
-                    grid-cols-2
-                    gap-3
-                "
-            >
-
-                {
-
-                    dashboardActions.map(action => (
-
-                        <ActionCard
-                            key={action.id}
-                            action={action}
-                        />
-
-                    ))
-
-                }
-
+            <div className="space-y-3">
+                {dashboardActions.map((action) => (
+                    <ActionCard
+                        key={action.id}
+                        action={action}
+                    />
+                ))}
             </div>
-
         </DashboardSection>
-
     );
-
 }
 
 export default QuickActionsSection;
