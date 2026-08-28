85f4817d0c7a6d8441c7f394d70b3f8e085a65dd
petermagaga <ptrmagaga@gmail.com>
2026-08-28 13:17:56 +0300

    fix: email

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -148,22 +148,3 @@ REST_FRAMEWORK = {
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
-EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_PORT = config("EMAIL_PORT", cast=int)
-
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-
-EMAIL_USE_TLS = config(
-    "EMAIL_USE_TLS",
-    default=True,
-    cast=bool
-)
-
-EMAIL_USE_SSL = False
-
-DEFAULT_FROM_EMAIL = config(
-    "DEFAULT_FROM_EMAIL"
-)
