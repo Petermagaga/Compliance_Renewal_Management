6d7d40268cae2fe3d52c31b990dedec329f24dd9
petermagaga <ptrmagaga@gmail.com>
2026-08-31 12:59:33 +0300

    forgotten password model

--- backend/authentication/serializers/password_serializer.py
+++ backend/authentication/serializers/password_serializer.py
@@ -17,3 +17,35 @@ class ChangePasswordSerializer(serializers.Serializer):
                 "Passwords do not match."
             )
         return attrs
+
+class ForgotPasswordSerializer(serializers.Serializer):
+    """
+    Validate a forgot password request
+    """
+    email=serializers.EmailField()
+
+class ResetPasswordSerializer(serializers.Serializer):
+    """
+    validate a password reset request
+    """
+
+    uid=serializers.CharField()
+    token=serializers.CharField()
+
+    new_password = serializers.CharField(
+        write_only=True,
+        min_length=8,
+    )
+
+    confirm_password=serializers.CharField(
+        write_only=True,
+        min_length=8
+    )
+
+    def validate(self,attrs):
+
+        if attrs["new_password"] != attrs["confirm_password"]:
+            raise serializers.ValidationError(
+                "passwords do not match."
+            )
+        return attrs
