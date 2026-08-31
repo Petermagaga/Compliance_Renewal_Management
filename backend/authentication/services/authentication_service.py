e820a68ad13dff560151d5b08228d89782dc0aaf
petermagaga <ptrmagaga@gmail.com>
2026-08-31 13:06:35 +0300

    forgotten password model

--- backend/authentication/services/authentication_service.py
+++ backend/authentication/services/authentication_service.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import authenticate
 from django.utils import timezone
-
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from authentication.models import User
 class AuthenticationService:
     """
     Handles all authentication business workflows
