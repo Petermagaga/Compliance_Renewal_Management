45d61ed5178ed59f118ef869f9a4bc2fb0eb9c37
petermagaga <ptrmagaga@gmail.com>
2026-09-21 15:34:08 +0300

    security_

--- backend/compliance/admin.py
+++ backend/compliance/admin.py
@@ -1,5 +1,34 @@
 from django.contrib import admin
-from .models import ComplianceItem,ReminderLog
 
-admin.site.register(ComplianceItem)
-admin.site.register(ReminderLog)
+from .models import ComplianceItem, ReminderLog
+
+
+@admin.register(ComplianceItem)
+class ComplianceItemAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+
+        # System super admins can see everything
+        if request.user.is_superuser:
+            return queryset
+
+        # Company admins/staff only see their own company's items
+        return queryset.filter(company=request.user.company)
+
+
+@admin.register(ReminderLog)
+class ReminderLogAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+
+        # System super admins can see everything
+        if request.user.is_superuser:
+            return queryset
+
+        # Company admins/staff only see reminders
+        # belonging to their company's compliance items
+        return queryset.filter(
+            compliance_item__company=request.user.company
+        )
